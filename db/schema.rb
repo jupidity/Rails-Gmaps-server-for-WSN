3e3f3c1e7012d998750c1d415bf3a48e70229a4c
Sean Cassero <sacassero@gmail.com>
2016-04-23 19:42:53 -0700

    Add time stamps to reports

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424021228) do
+ActiveRecord::Schema.define(version: 20160424024127) do
 
   create_table "reports", force: :cascade do |t|
-    t.float  "temp"
-    t.float  "lat"
-    t.float  "long"
-    t.string "sensor_mac"
-    t.string "controller_mac"
+    t.float    "temp"
+    t.float    "lat"
+    t.float    "long"
+    t.string   "sensor_mac"
+    t.string   "controller_mac"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "sensors", force: :cascade do |t|
